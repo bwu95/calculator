c3b730e2a577d43e66a1e059dff2886900e63502
Bright Wu <brightwu1995@gmail.com>
2023-12-11 11:17:55 -0800

    add delete functionality/handling of divison by 0

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-let total = null; 
-let num2 = null; 
+let firstOperand = null; 
+let secondOperand = null; 
 let operator = ''; 
 let userInput = ''; 
 let decimalClicked = false; 
+let errorMsg = 'NaN'; 
 
 let operatorButtons = document.querySelectorAll('.operation'); 
 let numberButton = document.querySelectorAll('.num');           //nodelist of all numbers  
-let clearButton = document.getElementById('clr-btn');           
+let clearButton = document.getElementById('clr-btn');     
+let deleteButton = document.getElementById('dlt-btn');          //delete DOM element       
 
 function add(num1, num2) {
     return num1 + num2; 
@@ -54,6 +56,10 @@ numberButton.forEach(function(button) {
         if (button.textContent === '.' && decimalClicked) {
             return; 
         }
+        
+        if(firstOperand !== null && operator == '') {       //user does operation on two number, then press number with no operation
+            firstOperand = null; 
+        }
 
         userInput += button.textContent; 
 
@@ -69,27 +75,41 @@ numberButton.forEach(function(button) {
 operatorButtons.forEach(function(operatorButton) {
     operatorButton.addEventListener('click', function() {   
 
-        if(operator === '') {
+        if(operator === '' && operatorButton.textContent !== '=') {                       
             operator = operatorButton.textContent; 
             
-            if(total === null) {
-                total = parseFloat(userInput); 
+            if(firstOperand === null) {
+                firstOperand = parseFloat(userInput); 
                 userInput = '';  
+                decimalClicked = false; 
             }
         }
-        else if(operatorButton.textContent === '=' && total) {
-             num2 = parseFloat(userInput); 
-             total = operate(total, num2, operator); 
-             displayVal(total); 
-             operator = ''; 
-             userInput = ''; 
+        else if(operator === '' && operatorButton.textContent === '=') {
+            return; 
         }
-        else {
-            num2 = parseFloat(userInput); 
-            total = operate(total, num2, operator); 
-            displayVal(total); 
+        else if(operator !== '' && userInput === '') {                          //change operation if there was initial operation and no second operand
             operator = operatorButton.textContent; 
+        }
+        else if(firstOperand && parseFloat(userInput) === 0) {
+            displayVal(errorMsg); 
+        }
+        else if(operatorButton.textContent === '=' && firstOperand) {           //when user enters = for doing calculation
+             secondOperand = parseFloat(userInput); 
+             firstOperand = operate(firstOperand, secondOperand, operator);     
+             displayVal(firstOperand); 
+             secondOperand = null; 
+             operator = '';                                                     //reset operator 
+             userInput = ''; 
+             decimalClicked = false; 
+        }
+        else {                                                                          //when user clicks operation other than = 
+            secondOperand = parseFloat(userInput); 
+            firstOperand = operate(firstOperand, secondOperand, operator);      //first operand becomes value of operation on two numbers
+            displayVal(firstOperand);                        
+            operator = operatorButton.textContent;                              //if user did not click =, make new operation operatorbutton.textContent
+            secondOperand = null; 
             userInput = ''; 
+            decimalClicked = false; 
         }
 
     })
@@ -97,13 +117,35 @@ operatorButtons.forEach(function(operatorButton) {
 
 //reset all values when clearing the screen 
 clearButton.addEventListener('click', function() {
-    total = null; 
-    num2 = null;  
+    firstOperand = null; 
+    secondOperand = null;  
     userInput = ''; 
     operator = ''; 
+    decimalClicked = false;
     displayVal('0'); 
 
-})
+}) 
+
+//delete most recent number a user has clicked 
+deleteButton.addEventListener('click', function() {
+    if(userInput.length === 0 && firstOperand !== null) {       //check if no input for second operand, delete first operand
+        firstOperand = firstOperand.toString().slice(0, -1); 
+        if (firstOperand === '') {
+            firstOperand = null;
+        } else {
+            firstOperand = parseFloat(firstOperand); 
+        }
+        displayVal(firstOperand); 
+    }
+    else if(userInput.length > 0) {
+        userInput = userInput.slice(0, -1); 
+        displayVal(userInput); 
+    }
+    //allow user to click decimal again if it gets deleted 
+    if(userInput.indexOf('.') === -1) {
+        decimalClicked = false; 
+    }
+} )
 
 function displayVal(val) {
     document.querySelector('.screen h1').textContent = val; 
